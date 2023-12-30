1f7d26fee9a392e910b52c4c2665f042f2f6f3eb
oldmanz <travis@oldmanz.com>
2023-12-30 18:05:42 -0500

    chocifi

---
+++ keyboards/crkbd/rev1/keymaps/zia/rules.mk
@@ -0,0 +1,8 @@
+VIA_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
+CONSOLE_ENABLE = no     # Console for debug(+400)
+AUDIO_ENABLE   = no     # Audio output on port C6
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE  = no
+LTO_ENABLE = yes
